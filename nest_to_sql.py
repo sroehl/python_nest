2edd17877216193b2588c525be0fdc790d0a8431
Steve Roehl <sroehl@cleo.com>
2015-06-29 13:39:13 -0500

    - Uses only the first device (only have one device in my setup). - Prints data needed, now should be added to SQL - Fixed issue that prevented importing of nest

--- nest_to_sql.py
+++ nest_to_sql.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#import nest
+import nest
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
@@ -9,16 +9,9 @@ config.read("credentials.config")
 
 username = config.get('credentials','username')
 password = config.get('credentials','password')
-print username
-print password
 
 napi = nest.Nest(username, password)
 
-for structure in napi.structures:
-    print 'Structure %s' % structure.name
-    print '    Away: %s' % structure.away
-    print '    Devices:'
 
-    for device in structure.devices:
-        print 'Device: %s' % device.name
-        print 'Temp: %0.1f' % nest.utils.c_to_f(device.temperature)
+print 'Away: %s' % napi.structures[0].away
+print 'Temp: %0.1f' % nest.utils.c_to_f(napi.structures[0].devices[0].temperature)
