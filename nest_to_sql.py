766996d781e9f41f7b851114ddbda845d6de48d3
Steven Roehl <stevenroehl@gmail.com>
2015-06-29 22:27:11 -0500

    Gets all of the values and commits to the database

--- nest_to_sql.py
+++ nest_to_sql.py
@@ -2,6 +2,11 @@
 
 import nest
 import ConfigParser
+import sqlite3
+import time
+
+conn = sqlite3.connect('nest_data.db')
+cur = conn.cursor()
 
 config = ConfigParser.ConfigParser()
 config.read("credentials.config")
@@ -11,7 +16,34 @@ username = config.get('credentials','username')
 password = config.get('credentials','password')
 
 napi = nest.Nest(username, password)
+home = napi.structures[0]
 
+while 1:
+	print "running"
+	napi = nest.Nest(username, password)
+	home = napi.structures[0]
+	curr_time = home.weather.current.datetime.strftime('%Y-%m-%d %H:%M:%S')
+	temp = nest.utils.c_to_f(home.devices[0].temperature)
+	outside_temp = nest.utils.c_to_f(home.weather.current.temperature)
+	humidity = home.devices[0].humidity
+	outside_humidity = home.weather.current.humidity
+	if home.away == False:
+		away = 0
+	else:
+		away = 1
+	if home.devices[0].fan == False:
+		fan = 0
+	else:
+		fan = 1
+		
+	if home.devices[0].mode == 'cool':
+		mode = 0
+	else:
+		mode = 1
+		
+	#print 'insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode)
 
-print 'Away: %s' % napi.structures[0].away
-print 'Temp: %0.1f' % nest.utils.c_to_f(napi.structures[0].devices[0].temperature)
+	cur.execute('insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode))
+	conn.commit()
+	time.sleep(60)
+
