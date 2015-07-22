c414fe73f3f8ddecd08c4bf64eabbdb3c8b6a83f
Steven Roehl <stevenroehl@gmail.com>
2015-07-21 19:28:19 -0500

    Added logic try except block to prevent the entire thing from crashing if there was a network error.

--- nest_to_sql.py
+++ nest_to_sql.py
@@ -15,8 +15,8 @@ config.read("credentials.config")
 username = config.get('credentials','username')
 password = config.get('credentials','password')
 
-napi = nest.Nest(username, password)
-home = napi.structures[0]
+#napi = nest.Nest(username, password)
+#home = napi.structures[0]
 
 
 def alreadyInserted(curr_time):
@@ -29,31 +29,34 @@ def alreadyInserted(curr_time):
 
 while 1:
 	#print "running"
-	napi = nest.Nest(username, password)
-	home = napi.structures[0]
-	curr_time = home.weather.current.datetime.strftime('%Y-%m-%d %H:%M:%S')
-	temp = nest.utils.c_to_f(home.devices[0].temperature)
-	outside_temp = nest.utils.c_to_f(home.weather.current.temperature)
-	humidity = home.devices[0].humidity
-	outside_humidity = home.weather.current.humidity
-	if home.away == False:
-		away = 0
-	else:
-		away = 1
-	if home.devices[0].fan == False:
-		fan = 0
-	else:
-		fan = 1
+	try:
+		napi = nest.Nest(username, password)
+		home = napi.structures[0]
+		curr_time = home.weather.current.datetime.strftime('%Y-%m-%d %H:%M:%S')
+		temp = nest.utils.c_to_f(home.devices[0].temperature)
+		outside_temp = nest.utils.c_to_f(home.weather.current.temperature)
+		humidity = home.devices[0].humidity
+		outside_humidity = home.weather.current.humidity
+		if home.away == False:
+			away = 0
+		else:
+			away = 1
+		if home.devices[0].fan == False:
+			fan = 0
+		else:
+			fan = 1
+			
+		if home.devices[0].mode == 'cool':
+			mode = 0
+		else:
+			mode = 1
+			
+		#print 'insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode)
 		
-	if home.devices[0].mode == 'cool':
-		mode = 0
-	else:
-		mode = 1
-		
-	#print 'insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode)
-	
-	if not alreadyInserted(curr_time):
-		cur.execute('insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode))
-		conn.commit()
-	time.sleep(60)
+		if not alreadyInserted(curr_time):
+			cur.execute('insert into data values ("%s",%d,%d,%d,%d,%i,%i,%i)' % (curr_time,temp,outside_temp,humidity,outside_humidity,away,fan,mode))
+			conn.commit()
+		time.sleep(60)
+	except:
+		time.sleep(120)
 
